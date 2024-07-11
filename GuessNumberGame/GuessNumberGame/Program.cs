15345370e90f966462ae08824f9e759a184b82ab
DmitryDanchenko <58415424+DmitryDanchenko@users.noreply.github.com>
2024-07-11 16:28:30 +0300

    fix

---
+++ GuessNumberGame/GuessNumberGame/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var number = new Random().Next(0,10);
+Console.WriteLine(number.ToString());
+
+Console.WriteLine("Try guess number");
+
+while (true)
+{
+    var userNumber = Convert.ToInt32(Console.ReadLine());
+    if (userNumber != number)
+    {
+        Console.WriteLine($"You're wrong, it's not {userNumber} \nplease try again");
+        continue;
+    }
+
+    Console.WriteLine($"You're right, it's real {number}");
+    break;
+}
