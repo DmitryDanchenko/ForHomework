2912dc639b11af57444402f1827e08577b05b422
DmitryDanchenko <58415424+DmitryDanchenko@users.noreply.github.com>
2024-08-02 12:44:45 +0300

    fix

---
+++ homeWork3/F.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork3
+{
+    internal class F
+    {
+        public int MyInt;
+        public string MyString;
+        public static F Get => new F { MyInt = 1, MyString = "Foo" };
+
+        public string ToCsv(object o)
+        {
+            return string.Join("\n", o.GetType().GetFields().Select(x => x.Name + "," + x.GetValue(o)));
+        }
+
+        public void WriteInFile(string str)
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory + "serializationData";
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+                {
+                    sw.Write(str);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public F ReadFile()
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory + "serializationData";
+            var deserializedClass = new F();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string[] array = new string[2];
+                string line;
+                line = sr.ReadLine();
+                array = line.Split(',');
+                var propertyInfo = deserializedClass.GetType().GetField(array[0]);
+                propertyInfo.SetValue(deserializedClass, Int32.Parse(array[1]));
+                line = sr.ReadLine();
+                array = line.Split(',');
+                var propertyInfo1 = deserializedClass.GetType().GetField(array[0]);
+                propertyInfo1.SetValue(deserializedClass, (array[1]).ToString());
+            }
+
+            return deserializedClass;
+        }
+
+        public override string ToString()
+        {
+            return ("MyInt=" + MyInt.ToString() + "\tMySting=" + MyString);
+        }
+    }
+}
+
---
+++ homeWork3/Program.cs
@@ -0,0 +1,63 @@
+﻿
+using homeWork3;
+using System.Diagnostics;
+
+string s = null;
+var c = F.Get;
+var stopWatch = new Stopwatch();
+//собственный механизм сериализации
+stopWatch.Start();
+for (int i = 0; i < 1000000; i++)
+{
+    s = c.ToCsv(c);
+}
+stopWatch.Stop();
+Console.WriteLine("Собственный механизм сериализации: " + stopWatch.Elapsed);
+stopWatch.Reset();
+Console.WriteLine(s);
+Console.WriteLine();
+c.WriteInFile(c.ToCsv(c));
+
+//замер: время вывода в консоль
+stopWatch.Start();
+Console.WriteLine(c.ToCsv(c));
+stopWatch.Stop();
+Console.WriteLine("Время вывода информации в консоль: " + stopWatch.Elapsed);
+stopWatch.Reset();
+Console.WriteLine();
+
+//сериализация с помощью средств Newtonsoft
+stopWatch.Start();
+for (int i = 0; i < 1000000; i++)
+{
+    Newtonsoft.Json.JsonConvert.SerializeObject(c);
+}
+stopWatch.Stop();
+Console.WriteLine("Механизм сериализации Newtonsoft: " + stopWatch.Elapsed);
+stopWatch.Reset();
+Console.WriteLine();
+
+//десериализация с помощью средств Newtonsoft
+var des = Newtonsoft.Json.JsonConvert.SerializeObject(c);
+stopWatch.Start();
+for (int i = 0; i < 1000000; i++)
+{
+    Newtonsoft.Json.JsonConvert.DeserializeObject(des);
+}
+stopWatch.Stop();
+Console.WriteLine("Механизм десериализации Newtonsoft: " + stopWatch.Elapsed);
+stopWatch.Reset();
+Console.WriteLine(des);
+Console.WriteLine();
+
+//собственная реализация десериализации
+stopWatch.Start();
+for (int i = 0; i < 1000; i++)
+{
+    var a = c.ReadFile();
+}
+stopWatch.Stop();
+Console.WriteLine("Механизм десериализации из файла: " + stopWatch.Elapsed);
+stopWatch.Reset();
+
+Console.ReadLine();
